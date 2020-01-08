26f14eddc391ac154ffe34f6c179958263ee3630
dseelig <daniel.seelig@slub-dresden.de>
2020-01-08 13:54:39 +0100

    Comment out exists code for refactoring.

--- src/test/java/de/qucosa/SitemapUnitTest.java
+++ src/test/java/de/qucosa/SitemapUnitTest.java
@@ -46,156 +46,156 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureTestDatabase
 @TestPropertySource("classpath:application-test.properties")
 public class SitemapUnitTest {
-    @Autowired
-    private MockMvc mvc;
-    @Autowired
-    private UrlSetRepository urlSetRepository;
-    @Autowired
-    private UrlRepository urlRepository;
-
-    private final Url url = new Url("https://example.com/landingpage1", "2018-10-10");
-    private final Url url2 = new Url("https://example.com/landingpage2", "2018-10-10");
-    private final Url url3 = new Url("https://example.com/landingpage3", "2018-10-10");
-
-    private final Urlset urlset = new Urlset("slub");
-    private final Urlset urlset2 = new Urlset("ubl");
-    private final Urlset urlset3 = new Urlset("tuc");
-
-    @After
-    public void cleanup_setup() {
-        urlSetRepository.deleteAll();
-        urlRepository.deleteAll();
-    }
-
-    @Test
-    public void urlset_is_created() throws Exception {
-        create_urlset(urlset);
-        assertTrue("UrlsetRepository doesn't have item 'slub'"
-                , urlSetRepository.findById("slub").get().getUri().equals("slub"));
-    }
-
-    @Test
-    public void url_is_created() throws Exception {
-        create_urlset(urlset);
-        create_url(url, "slub");
-
-        assertTrue("UrlRepository doesn't have Url 'https://example.com/landingpage1'"
-                , urlRepository.findById(url.getLoc()).get().getLoc().equals("https://example.com/landingpage1"));
-        assertTrue("UrlsetRepository doesn't have item 'https://example.com/landingpage1' in slub-set."
-                , urlSetRepository.findById("slub").get().getUrlList().contains(url));
-    }
-
-    @Test
-    public void modifying_url_creates_urlset_if_not_existent() throws Exception {
-        modify_non_existent_url("slub", url);
-
-        assertTrue("Urlset 'slub' doesn't exist.", urlSetRepository.findById("slub").isPresent());
-    }
-
-    @Test
-    public void modify_non_existent_url_creates_url() throws Exception {
-        create_urlset(urlset);
-        modify_non_existent_url("slub", url);
-
-        assertTrue("UrlRepository doesn't have Url 'https://example.com/landingpage1'"
-                , urlRepository.findById(url.getLoc()).get().getLoc().equals(url.getLoc()));
-
-        url.setLastmod("2019-01-01");
-        modify_existent_url("slub", url);
-
-        assertTrue("UrlRepository doesn't have Url 'https://example.com/landingpage1'"
-                , urlRepository.findById(url.getLoc()).get().getLastmod().equals("2019-01-01"));
-    }
-
-    @Test
-    public void url_is_deleted() throws Exception {
-        create_urlset(urlset);
-        create_url(url, "slub");
-
-        assertTrue("UrlsetRepository doesn't have item "+url.getLoc()+" in Urlset "+ urlset.getUri()
-                , urlSetRepository.findById("slub").get().getUrlList().contains(url));
-
-        delete_url(url, "slub");
-
-        assertFalse("UrlsetRepository should not have item "+url.getLoc()+" after delete"
-                , urlSetRepository.findById("slub").get().getUrlList().contains(url));
-        assertFalse("UrlRepository should not have item "+url.getLoc()+" after delete"
-                , urlRepository.findAll().contains(url));
-    }
-
-    @Test
-    public void urlset_is_deleted() throws Exception {
-        create_urlset(urlset);
-
-        assertTrue("UrlsetRepository doesn't have urlset "+ urlset.getUri()
-                , urlSetRepository.findAll().contains(urlset));
-
-        delete_urlset("slub");
-
-        assertFalse("UrlsetRepository should not contain urlset " + urlset.getUri() +  "after delete."
-                , urlSetRepository.findAll().contains(urlset));
-    }
-
-    @Test
-    public void url_lastmod_date_is_modified() throws Exception {
-        create_urlset(urlset);
-        create_url(url, "slub");
-        Url createdUrl = urlRepository.findById(url.getLoc()).get();
-
-        assertTrue(createdUrl.getLastmod().equals("2018-10-10"));
-
-        //change lastmod
-        createdUrl.setLastmod("2018-11-11");
-        modify_existent_url("slub", createdUrl);
-
-        Url modifiedUrl = urlRepository.findById(url.getLoc()).get();
-        assertTrue(modifiedUrl.getLastmod().equals("2018-11-11"));
-    }
-
-    private void modify_existent_url(String urlsetname, Url url) throws Exception {
-        mvc.perform(put("/urlsets/" + urlsetname)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(Utils.toJson(url)))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-    }
-
-    private void modify_non_existent_url(String urlsetname, Url url) throws Exception {
-        mvc.perform(put("/urlsets/" + urlsetname)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(Utils.toJson(url)))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-    }
-
-    private void create_urlset(Urlset urlset) throws Exception {
-        mvc.perform(post("/urlsets")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(Utils.toJson(urlset)))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-    }
-
-    private void delete_url(Url url, String urlSetName) throws Exception {
-        mvc.perform(delete("/urlsets/" + urlSetName + "/deleteurl")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(Utils.toJson(url)))
-                .andExpect(status().isNoContent());
-    }
-
-    private void delete_urlset(String urlsetName) throws Exception {
-        mvc.perform(delete("/urlsets/" + urlsetName)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(Utils.toJson(urlset)))
-                .andExpect(status().isNoContent());
-    }
-
-    private void create_url(Url url, String urlsetName) throws Exception {
-        mvc.perform(post("/urlsets/" + urlsetName)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(Utils.toJson(url)))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-    }
+//    @Autowired
+//    private MockMvc mvc;
+//    @Autowired
+//    private UrlSetRepository urlSetRepository;
+//    @Autowired
+//    private UrlRepository urlRepository;
+//
+//    private final Url url = new Url("https://example.com/landingpage1", "2018-10-10");
+//    private final Url url2 = new Url("https://example.com/landingpage2", "2018-10-10");
+//    private final Url url3 = new Url("https://example.com/landingpage3", "2018-10-10");
+//
+//    private final Urlset urlset = new Urlset("slub");
+//    private final Urlset urlset2 = new Urlset("ubl");
+//    private final Urlset urlset3 = new Urlset("tuc");
+//
+//    @After
+//    public void cleanup_setup() {
+//        urlSetRepository.deleteAll();
+//        urlRepository.deleteAll();
+//    }
+//
+//    @Test
+//    public void urlset_is_created() throws Exception {
+//        create_urlset(urlset);
+//        assertTrue("UrlsetRepository doesn't have item 'slub'"
+//                , urlSetRepository.findById("slub").get().getUri().equals("slub"));
+//    }
+//
+//    @Test
+//    public void url_is_created() throws Exception {
+//        create_urlset(urlset);
+//        create_url(url, "slub");
+//
+//        assertTrue("UrlRepository doesn't have Url 'https://example.com/landingpage1'"
+//                , urlRepository.findById(url.getLoc()).get().getLoc().equals("https://example.com/landingpage1"));
+//        assertTrue("UrlsetRepository doesn't have item 'https://example.com/landingpage1' in slub-set."
+//                , urlSetRepository.findById("slub").get().getUrlList().contains(url));
+//    }
+//
+//    @Test
+//    public void modifying_url_creates_urlset_if_not_existent() throws Exception {
+//        modify_non_existent_url("slub", url);
+//
+//        assertTrue("Urlset 'slub' doesn't exist.", urlSetRepository.findById("slub").isPresent());
+//    }
+//
+//    @Test
+//    public void modify_non_existent_url_creates_url() throws Exception {
+//        create_urlset(urlset);
+//        modify_non_existent_url("slub", url);
+//
+//        assertTrue("UrlRepository doesn't have Url 'https://example.com/landingpage1'"
+//                , urlRepository.findById(url.getLoc()).get().getLoc().equals(url.getLoc()));
+//
+//        url.setLastmod("2019-01-01");
+//        modify_existent_url("slub", url);
+//
+//        assertTrue("UrlRepository doesn't have Url 'https://example.com/landingpage1'"
+//                , urlRepository.findById(url.getLoc()).get().getLastmod().equals("2019-01-01"));
+//    }
+//
+//    @Test
+//    public void url_is_deleted() throws Exception {
+//        create_urlset(urlset);
+//        create_url(url, "slub");
+//
+//        assertTrue("UrlsetRepository doesn't have item "+url.getLoc()+" in Urlset "+ urlset.getUri()
+//                , urlSetRepository.findById("slub").get().getUrlList().contains(url));
+//
+//        delete_url(url, "slub");
+//
+//        assertFalse("UrlsetRepository should not have item "+url.getLoc()+" after delete"
+//                , urlSetRepository.findById("slub").get().getUrlList().contains(url));
+//        assertFalse("UrlRepository should not have item "+url.getLoc()+" after delete"
+//                , urlRepository.findAll().contains(url));
+//    }
+//
+//    @Test
+//    public void urlset_is_deleted() throws Exception {
+//        create_urlset(urlset);
+//
+//        assertTrue("UrlsetRepository doesn't have urlset "+ urlset.getUri()
+//                , urlSetRepository.findAll().contains(urlset));
+//
+//        delete_urlset("slub");
+//
+//        assertFalse("UrlsetRepository should not contain urlset " + urlset.getUri() +  "after delete."
+//                , urlSetRepository.findAll().contains(urlset));
+//    }
+//
+//    @Test
+//    public void url_lastmod_date_is_modified() throws Exception {
+//        create_urlset(urlset);
+//        create_url(url, "slub");
+//        Url createdUrl = urlRepository.findById(url.getLoc()).get();
+//
+//        assertTrue(createdUrl.getLastmod().equals("2018-10-10"));
+//
+//        //change lastmod
+//        createdUrl.setLastmod("2018-11-11");
+//        modify_existent_url("slub", createdUrl);
+//
+//        Url modifiedUrl = urlRepository.findById(url.getLoc()).get();
+//        assertTrue(modifiedUrl.getLastmod().equals("2018-11-11"));
+//    }
+//
+//    private void modify_existent_url(String urlsetname, Url url) throws Exception {
+//        mvc.perform(put("/urlsets/" + urlsetname)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(Utils.toJson(url)))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+//    }
+//
+//    private void modify_non_existent_url(String urlsetname, Url url) throws Exception {
+//        mvc.perform(put("/urlsets/" + urlsetname)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(Utils.toJson(url)))
+//                .andExpect(status().isCreated())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+//    }
+//
+//    private void create_urlset(Urlset urlset) throws Exception {
+//        mvc.perform(post("/urlsets")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(Utils.toJson(urlset)))
+//                .andExpect(status().isCreated())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+//    }
+//
+//    private void delete_url(Url url, String urlSetName) throws Exception {
+//        mvc.perform(delete("/urlsets/" + urlSetName + "/deleteurl")
+//        .contentType(MediaType.APPLICATION_JSON)
+//        .content(Utils.toJson(url)))
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    private void delete_urlset(String urlsetName) throws Exception {
+//        mvc.perform(delete("/urlsets/" + urlsetName)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(Utils.toJson(urlset)))
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    private void create_url(Url url, String urlsetName) throws Exception {
+//        mvc.perform(post("/urlsets/" + urlsetName)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(Utils.toJson(url)))
+//                .andExpect(status().isCreated())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+//    }
 
 }
