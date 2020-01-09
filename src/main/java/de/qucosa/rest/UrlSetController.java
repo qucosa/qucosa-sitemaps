9a5fdabf7653403ab6225fc79739013da8798ea2
dseelig <daniel.seelig@slub-dresden.de>
2020-01-09 12:40:48 +0100

    Add HttpServletRequest object as param in save process. Add check if uri value in urlset object not empty. Generate date and loc value.

--- src/main/java/de/qucosa/rest/UrlSetController.java
+++ src/main/java/de/qucosa/rest/UrlSetController.java
@@ -4,6 +4,8 @@ import de.qucosa.ErrorDetails;
 import de.qucosa.repository.exceptions.SaveFailed;
 import de.qucosa.repository.model.UrlSet;
 import de.qucosa.repository.services.UrlSetService;
+import de.qucosa.utils.Utils;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,19 +15,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/urlsets")
 public class UrlSetController {
     private UrlSetService urlSetService;
 
-    public UrlSetController(UrlSetService urlSetService) {
+    private Environment environment;
+
+    public UrlSetController(UrlSetService urlSetService, Environment environment) {
         this.urlSetService = urlSetService;
+        this.environment = environment;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    public ResponseEntity createUrlSet(@RequestBody UrlSet urlset) {
+    public ResponseEntity createUrlSet(@RequestBody UrlSet urlset, HttpServletRequest request) {
         UrlSet output = null;
+        String hosturl = request.getRequestURL().toString();
+
+        if (urlset.getUri().isEmpty()) {
+            return new ErrorDetails(this.getClass().getName(), "createUrlSet", "POST:urlsets",
+                    HttpStatus.BAD_REQUEST, "UrlSet uri ist empty.", null).response();
+        }
+
+        urlset.setLoc(hosturl + "/" + urlset.getUri());
+        urlset.setLastmod(Utils.getCurrentW3cDatetime());
 
         try {
             output = urlSetService.saveUrlSet(urlset);
