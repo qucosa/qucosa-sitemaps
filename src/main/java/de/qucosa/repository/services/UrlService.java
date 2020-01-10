b024fd782d29fbcd291145f30f8d136bcb5b1224
dseelig <daniel.seelig@slub-dresden.de>
2020-01-10 08:54:30 +0100

    Add save url method.

--- src/main/java/de/qucosa/repository/services/UrlService.java
+++ src/main/java/de/qucosa/repository/services/UrlService.java
@@ -1,6 +1,7 @@
 package de.qucosa.repository.services;
 
 import de.qucosa.repository.Dao;
+import de.qucosa.repository.exceptions.SaveFailed;
 import de.qucosa.repository.model.Url;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,9 @@ public class UrlService {
 
     public void setDao(Dao<Url> dao) {
         this.dao = dao;
+    }
+
+    public Url save(Url url) throws SaveFailed {
+        return dao.saveAndSetIdentifier(url);
     }
 }
