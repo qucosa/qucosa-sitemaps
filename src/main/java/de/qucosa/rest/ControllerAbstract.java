b4f3196d2be3e34676b39ae13843138b869dfc7a
dseelig <daniel.seelig@slub-dresden.de>
2020-01-15 12:38:33 +0100

    Add server port property.

--- src/main/java/de/qucosa/rest/ControllerAbstract.java
+++ src/main/java/de/qucosa/rest/ControllerAbstract.java
@@ -1,10 +1,14 @@
 package de.qucosa.rest;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import javax.servlet.http.HttpServletRequest;
 
 public abstract class ControllerAbstract {
+    @Value("${server.port}")
+    protected int serverPort;
 
-    protected String serverUrl(HttpServletRequest request) {
-        return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+    protected String serverUrl(HttpServletRequest request, int port) {
+        return request.getScheme() + "://" + request.getServerName() + ":" + port;
     }
 }
