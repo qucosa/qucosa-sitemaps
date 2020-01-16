55f665d26cf26ff2517a92becc86c14eda9ed34d
dseelig <daniel.seelig@slub-dresden.de>
2020-01-16 08:36:13 +0100

    Remove unused import.

--- src/main/java/de/qucosa/repository/dao/UrlSetDao.java
+++ src/main/java/de/qucosa/repository/dao/UrlSetDao.java
@@ -32,6 +32,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Repository
 public class UrlSetDao<T extends UrlSet> implements Dao<UrlSet> {
