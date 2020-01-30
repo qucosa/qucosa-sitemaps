6ea08297dd6abc4ae3b25d41007d81bc8f143499
dseelig <daniel.seelig@slub-dresden.de>
2020-01-30 08:18:20 +0100

    Add files for dockerize application. It will build two containers in docker-compose file.   - qucosa-sitemap-db container   - qucosa-sitemap container

--- sql-install-tables.sql
+++ sql-install-tables.sql
@@ -1,24 +1,3 @@
--- Table: public.url
--- DROP TABLE public.url;
-CREATE TABLE public.url
-(
-  loc character varying(255) NOT NULL,
-  changefreq character varying(255),
-  lastmod character varying(255),
-  priority character varying(255),
-  urlset_uri character varying(255),
-  CONSTRAINT url_pkey PRIMARY KEY (loc),
-  CONSTRAINT fkv3ogpa2udo3tpkco03v2yao3 FOREIGN KEY (urlset_uri)
-      REFERENCES public.urlset (uri) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE public.url
-  OWNER TO qucosa;
-
-
 -- Table: public.urlset
 -- DROP TABLE public.urlset;
 CREATE TABLE public.urlset
@@ -32,4 +11,25 @@ WITH (
   OIDS=FALSE
 );
 ALTER TABLE public.urlset
-  OWNER TO qucosa;
+  OWNER TO sitemap;
+
+
+-- Table: public.url
+-- DROP TABLE public.url;
+CREATE TABLE public.url
+(
+  loc character varying(255) NOT NULL,
+  changefreq character varying(255),
+  lastmod character varying(255),
+  priority character varying(255),
+  urlset_uri character varying(255),
+  CONSTRAINT url_pkey PRIMARY KEY (loc),
+  CONSTRAINT urlset_fkey FOREIGN KEY (urlset_uri)
+      REFERENCES public.urlset (uri) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.url
+  OWNER TO sitemap;
